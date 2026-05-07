a5f14bf70a4114d31f462e3fe985fb24af379f60
sarawalk <f.sarawalk@gmail.com>
2026-05-07 18:24:54 +0200

    bug fixed

--- qdecm_bowtie_sampler.py
+++ qdecm_bowtie_sampler.py
@@ -84,7 +84,7 @@ def main():
                 #if file_mtime == dt.date.today():
                 with open(qdecm_filename, 'rb') as f:
                     qdecm=pickle.load(f)
-                if hasattr(qdecm.sol, 'sol') and qdecm.sol.converged:
+                if hasattr(qdecm, 'sol') and qdecm.sol.converged:
                     print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] Processing DiCo {d} with {len(el_dico[d]):,} edges...')
                     sys.stdout.flush()
                     block_dict, flux_dict=validate(el_dico[dico_class], qdecm.sol, n_runs=2*1000)
