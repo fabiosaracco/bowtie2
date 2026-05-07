df38297d7a5a87a54c7a7ca5feb209ed2a12701f
sarawalk <f.sarawalk@gmail.com>
2026-05-07 18:39:33 +0200

    bug ficed

--- qdecm_bowtie_sampler.py
+++ qdecm_bowtie_sampler.py
@@ -87,7 +87,7 @@ def main():
                 if hasattr(qdecm, 'sol') and qdecm.sol.converged:
                     print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] Processing DiCo {d} with {len(el_dico[d]):,} edges...')
                     sys.stdout.flush()
-                    block_dict, flux_dict=validate(el_dico[d], qdecm.sol, n_runs=2*1000)
+                    block_dict, flux_dict=validate(el_dico[d], qdecm, n_runs=2*1000)
                     with open(PVALUE_FOLDER+f'{dataset_name}_dico{d}_pvalues_blocks.pkl', 'wb') as f:
                         pickle.dump(block_dict, f)
                     with open(PVALUE_FOLDER+f'{dataset_name}_dico{d}_pvalues_fluxes.pkl', 'wb') as f:
