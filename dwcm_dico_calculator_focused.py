f95bb8ce09e0b5d9771823507da3589c9c1a117d
sarawalk <f.sarawalk@gmail.com>
2026-07-16 11:56:42 +0200

    Update execution counts in model selection notebook and handle missing module error in DWCM calculator

---
+++ dwcm_dico_calculator_focused.py
@@ -0,0 +1,212 @@
+import os, sys
+import platform
+import numpy as np
+import pickle 
+import datetime as dt
+from collections import defaultdict
+
+from dcms.models import DCMModel, DECMModel, qDECMModel, DWCMModel
+
+from auxiliary_functions import el2ks
+
+# Set HOME directory based on the operating system
+if platform.system() == 'Darwin':
+    HOME = '/Users/fabio/Documents/Lavoro/PythonFiles/bowtie2_py310/bowtie2/'
+elif platform.system() == 'Linux':
+    HOME = '/home/sarawalk/bowtie2_py39/bowtie2/'
+else:
+    raise RuntimeError(f"Unsupported OS: {platform.system()}")
+
+sys.path.insert(0, HOME)
+DATA_FOLDER=HOME+'dati_elezioni/'
+
+DATASET='crisi'
+#DICO=0
+#DICO=1
+DICO=2
+#DICO=3
+
+
+MAX_TIME_HOURS=5
+MAX_ITER=5000
+TOL=1e-5
+ANDERSON=7
+HUB_TH=5
+GAMMA=0.
+MONITOR=False
+RECYCLE_SOL=True
+
+
+
+def main():
+    
+    dataset_keyword=DATASET
+    dico_class=DICO
+    
+    
+    files=os.listdir(DATA_FOLDER)
+    files.sort()
+
+    # Each dataset has two files:
+    # - ???_dicos.csv: DiCo information per node
+    # - ???_weighted_edgelist.csv: edge list with columns source_id, target_id, weight
+
+    # Focus on a single dataset
+    files=[f for f in files if f.startswith(dataset_keyword)]
+
+    dico_file = files[0]
+    el_file = files[1]
+    dataset_name=dico_file[:-10]
+    print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] ***{dataset_name.title()}***')
+
+    # Load the DiCo information
+    dico=np.genfromtxt(DATA_FOLDER+dico_file, delimiter=',',skip_header=1, autostrip=True, dtype=[('user_id', '>U50'), ('dico', '>U2'), ('h_dico', 'U2'), ('i_dico', 'U2')])
+        
+    # Load the edge list
+    el=np.genfromtxt(DATA_FOLDER+el_file, delimiter=',', skip_header=1,autostrip=True, dtype=[('source_id', '>U50'), ('target_id', '>U20'),('weight', 'i4')])
+
+    # Select correct dicos
+    print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] Selecting only interpretable dicos...')
+    sys.stdout.flush()
+    dico_dict={}
+    bad_dicos=[]
+    for d in dico:
+        if d['dico'].isnumeric():
+            dico_dict[d['user_id']]=int(d['dico'])
+        else:
+            if d['dico'] not in bad_dicos:
+                bad_dicos.append(d['dico'])
+
+    cacca=np.unique(list(dico_dict.values()), return_counts=True)
+    #print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] DiCo nodes distribution:') 
+    #for entry in np.vstack(cacca).T:
+    #    print(f'{entry[0]}, {entry[1]:7,d}')
+    #sys.stdout.flush()
+
+    # Nodes
+    n_nodes=np.concatenate((el['source_id'], el['target_id']))
+    n_nodes=np.unique(n_nodes)
+
+    #print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] N(nodes)={len(n_nodes):,}, N(nodes in dico)={len(dico_dict):,}, share={len(dico_dict)/len(n_nodes):.3f}')
+
+    # Edges
+    
+    _tmp = defaultdict(list)
+    # auxiliary defaultdict to group edges by dico class
+    for edge in el:
+        src = edge['source_id'].strip()
+        tgt = edge['target_id'].strip()
+        d_src = dico_dict.get(src)
+        if d_src is not None and d_src == dico_dict.get(tgt):
+            _tmp[d_src].append(edge)
+
+    el_dico = defaultdict(
+        lambda: np.empty(0, dtype=el.dtype),
+        {k: np.array(v, dtype=el.dtype) for k, v in _tmp.items()}
+    )
+
+    del _tmp
+
+    cacca=np.array([[key, len(el_dico[key])] for key in el_dico.keys()])
+
+    #print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] DiCo edges distribution:') 
+    dicos=list(el_dico.keys())
+    dicos.sort()
+    
+    #for d in dicos:
+    #    print(f'{d}, {len(el_dico[d]):8,d}')
+    #sys.stdout.flush()
+    
+    print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] Processing DiCo class {dico_class}...')
+    sys.stdout.flush()
+    aux=el2ks(el_dico[dico_class])
+    
+    # consistency checks: topology
+    assert aux[0].sum()==aux[1].sum()==len(el_dico[dico_class])
+    # consistency checks: weights
+    assert aux[2].sum()==aux[3].sum()
+
+    print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] N(nodes)={len(aux[4]):,}, N(edges)={len(el_dico[dico_class]):,}, density={len(el_dico[dico_class])/len(aux[4])**2:.2e}')
+    sys.stdout.flush()
+
+    if len(aux[4])>3*10**4:
+        print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] The network is too big for the actual technology. Skipping, but with the aim to tackle it in the near future...')
+        return
+    
+    file_name=HOME+f'/tests/{dataset_name}_dico{dico_class}_dwcm_final.pkl'
+    
+    if os.path.exists(file_name):
+        # check if the file was created/modified today
+        #file_mtime = dt.date.fromtimestamp(os.path.getmtime(file_name))
+        #if file_mtime == dt.date.today():
+        
+        with open(file_name, 'rb') as f:
+            old_dwcm=pickle.load(f)
+        if hasattr(old_dwcm, 'sol') and hasattr(old_dwcm.sol, 'converged') and old_dwcm.sol.converged and old_dwcm.sol.mre<TOL:
+            print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] A converged solution for DWCM with pytorch and theta already exists. Skipping...')
+            sys.stdout.flush()
+            return
+
+        print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] DWCM, pytorch, theta (max: {MAX_TIME_HOURS:} hours)')
+        dwcm=DWCMModel(aux[0], aux[1], aux[2], aux[3])
+
+        # check if the existing solution can be used as a starting point
+        if RECYCLE_SOL and hasattr(old_dwcm, 'sol'):
+            print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] Recycling existing non-converged solution...')
+            ic=old_dwcm.sol.best_theta
+        else:
+            # the previous run did not complete the evaluation
+            # and the existing solution cannot be used as a starting point
+            ic='strengths'
+        sys.stdout.flush()
+
+    else:
+        print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] DWCM, pytorch, theta (max: {MAX_TIME_HOURS:} hours)')
+        dwcm=DWCMModel(aux[0], aux[1], aux[2], aux[3])
+        ic='strengths'
+
+    
+    try:
+        dwcm.solve_tool(tol=TOL, backend='pytorch', ic=ic, max_time=MAX_TIME_HOURS*3600, max_iter=MAX_ITER, verbose=True, monitor=MONITOR, anderson_depth=ANDERSON)
+
+        # elapsed time (in hours and minutes)
+        t_ets=dwcm.sol.elapsed_time
+        eth=t_ets//3600
+        etm=(t_ets % 3600)/60
+    
+        if dwcm.sol.converged:
+            print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] DWCM converged in {int(eth):2d} h and {etm:2.2f} m, MRE={dwcm.sol.mre:.4e}, (peak RAM={dwcm.sol.peak_ram_bytes//1024**2} MB)')
+            
+        else:
+            print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] DWCM did not converge in {int(eth):2d} h and {etm:2.2f} m, MRE={dwcm.sol.mre:.4e}, (peak RAM={dwcm.sol.peak_ram_bytes//1024**2} MB)')
+        sys.stdout.flush()
+
+        final_file_name=file_name
+        counter=0
+        if os.path.exists(file_name):
+            final_file_name=file_name.replace('.pkl', f'_{counter}.pkl')
+        while os.path.exists(final_file_name):
+            counter+=1
+            final_file_name=file_name.replace('.pkl', f'_{counter}.pkl')
+        
+        print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] file name: {final_file_name}')
+        sys.stdout.flush()
+        with open(final_file_name, 'wb') as f:
+            pickle.dump(dwcm, f)
+        
+        print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] Saved!')
+        sys.stdout.flush()
+        
+    except Exception as e:
+        print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] Error solving DWCM with pytorch and theta: {e}')
+        sys.stdout.flush()
+        
+        
+    
+
+
+
+if __name__ == "__main__":
+    main()
+    if platform.system() == 'Darwin':
+        os.system("afplay ta-da.mp3")
