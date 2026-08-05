cd0ba95405ce2f0051e88d41731d92260e35e23c
sarawalk <f.sarawalk@gmail.com>
2026-08-05 15:04:08 +0200

    Refactor data loading and processing in decm_bowtie_sampler.py
    - Simplified the get_input_data function by separating the logic for loading DiCo files and edge lists into a new function, get_dicos.
    - Removed unnecessary print statements and comments to clean up the code.
    - Enhanced the main function to iterate over datasets and handle file existence checks more robustly.
    - Added logic to ensure convergence of DECM files before processing.
    - Updated file naming conventions for output files to include 'decm' for clarity.

--- decm_bowtie_sampler.py
+++ decm_bowtie_sampler.py
@@ -23,35 +23,21 @@ PVALUE_FOLDER=HOME+'pvalues/'
 
 #N_RUNS=2*1000
 N_RUNS=5*1000
+RERUN=False
 
-def get_input_data():
-    dataset_keyword=DATASET
-    dico_class=DICO
-        
-    files=os.listdir(DATA_FOLDER)
-    files.sort()
-    
-    # Each dataset has two files:
-    # - ???_dicos.csv: DiCo information per node
-    # - ???_weighted_edgelist.csv: edge list with columns source_id, target_id, weight
-    
-    # Focus on a single dataset
-    files=[f for f in files if f.startswith(dataset_keyword)]
-    
-    dico_file = files[0]
-    el_file = files[1]
+def get_dicos(files, i):
+    dico_file = files[2*i]
+    el_file = files[2*i + 1]
     dataset_name=dico_file[:-10]
     print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] ***{dataset_name.title()}***')
 
     # Load the DiCo information
     dico=np.genfromtxt(DATA_FOLDER+dico_file, delimiter=',',skip_header=1, autostrip=True, dtype=[('user_id', '>U50'), ('dico', '>U2'), ('h_dico', 'U2'), ('i_dico', 'U2')])
-        
+    
     # Load the edge list
     el=np.genfromtxt(DATA_FOLDER+el_file, delimiter=',', skip_header=1,autostrip=True, dtype=[('source_id', '>U50'), ('target_id', '>U20'),('weight', 'i4')])
 
     # Select correct dicos
-    print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] Selecting only interpretable dicos...')
-    sys.stdout.flush()
     dico_dict={}
     bad_dicos=[]
     for d in dico:
@@ -61,20 +47,12 @@ def get_input_data():
             if d['dico'] not in bad_dicos:
                 bad_dicos.append(d['dico'])
 
-    cacca=np.unique(list(dico_dict.values()), return_counts=True)
-    #print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] DiCo nodes distribution:') 
-    #for entry in np.vstack(cacca).T:
-    #    print(f'{entry[0]}, {entry[1]:7,d}')
-    #sys.stdout.flush()
-
     # Nodes
     n_nodes=np.concatenate((el['source_id'], el['target_id']))
     n_nodes=np.unique(n_nodes)
 
-    #print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] N(nodes)={len(n_nodes):,}, N(nodes in dico)={len(dico_dict):,}, share={len(dico_dict)/len(n_nodes):.3f}')
-
     # Edges
-    n_edges = len(el)
+    
     _tmp = defaultdict(list)
     # auxiliary defaultdict to group edges by dico class
     for edge in el:
@@ -91,54 +69,61 @@ def get_input_data():
 
     del _tmp
 
-    cacca=np.array([[key, len(el_dico[key])] for key in el_dico.keys()])
-
-    #print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] DiCo edges distribution:') 
     dicos=list(el_dico.keys())
     dicos.sort()
-    
-    #for d in dicos:
-    #    print(f'{d}, {len(el_dico[d]):8,d}')
-    #sys.stdout.flush()
-    
-    print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] Processing DiCo class {dico_class}...')
-    sys.stdout.flush()
-    aux=el2ks(el_dico[dico_class])
 
-    # consistency checks: topology
-    assert aux[0].sum()==aux[1].sum()==len(el_dico[dico_class])
-    # consistency checks: weights
-    assert aux[2].sum()==aux[3].sum()
+    return dicos, el_dico
 
-    return aux, n_edges, dataset_name
+
 
 def main():
 
-    aux, n_edges, dataset_name=get_input_data()
-        
-    
-    qdecm_filename=TEST_FOLDER+f'{dataset_name}_dico{d}_qdecm.pkl'
-    pvalue_block_filename=PVALUE_FOLDER+f'{dataset_name}_dico{d}_pvalues_blocks.pkl'
-    pvalue_flux_filename=PVALUE_FOLDER+f'{dataset_name}_dico{d}_pvalues_fluxes.pkl'
-    #if os.path.exists(pvalue_block_filename) and os.path.exists(pvalue_flux_filename):
-    #    print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] P-value files for DiCo {d} already exist, skipping...')
-    #    continue
-    counter=0
-    while os.path.exists(pvalue_block_filename):
-        pvalue_block_filename=PVALUE_FOLDER+f'{dataset_name}_dico{d}_pvalues_blocks_{counter}.pkl'
-        pvalue_flux_filename=PVALUE_FOLDER+f'{dataset_name}_dico{d}_pvalues_fluxes_{counter}.pkl'
-        counter+=1
-        
-    
-    if os.path.exists(qdecm_filename):
-        # check if the file was created/modified today
-        #file_mtime = dt.date.fromtimestamp(os.path.getmtime(qdecm_filename))
-        #if file_mtime == dt.date.today():
-        with open(qdecm_filename, 'rb') as f:
-            qdecm=pickle.load(f)
-        if hasattr(qdecm, 'sol') and qdecm.sol.converged:
+    files=os.listdir(DATA_FOLDER)
+    files.sort()
+
+    # Each dataset has two files:
+    # - ???_dicos.csv: DiCo information per node
+    # - ???_weighted_edgelist.csv: edge list with columns source_id, target_id, weight
+
+    for i in range(len(files)//2):
+        dicos, el_dico = get_dicos(files, i)
+        for d in dicos:
+
+            # find the proper input file
+            dataset_dico_files=[file for file in os.listdir(TEST_FOLDER) if f'{dataset_name}' in file and f'dico{d}' in file and 'decm' in file]
+            if len(dataset_dico_files) == 0:
+                print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] No DECM file found for DiCo {d}, skipping...')
+                sys.stdout.flush()
+                continue
+            else:
+                convergence_found=False
+                for file in dataset_dico_files:
+                    # the last one is the definitive one
+                    with open(TEST_FOLDER+file, 'rb') as f:
+                        _aux=pickle.load(f)
+                    if hasattr(_aux, 'sol') and _aux.sol.converged:
+                        convergence_found=True
+                        decm=_aux
+                if convergence_found==False:
+                    print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] No converged DECM file found for DiCo {d}, skipping...')
+                    sys.stdout.flush()
+                    continue
             print(f'[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] Processing DiCo {d}...')
             sys.stdout.flush()
+
+            # fidn the proper destination file
+            pvalue_block_filename=PVALUE_FOLDER+f'{dataset_name}_dico{d}_pvalues_blocks_decm.pkl'
+            pvalue_flux_filename=PVALUE_FOLDER+f'{dataset_name}_dico{d}_pvalues_fluxes_decm.pkl'
+
+                        
+
+            block_dict, flux_dict=validate(el_dico[d], decm, n_runs=N_RUNS, verbose=True)
+            counter=0
+            while os.path.exists(pvalue_block_filename):
+                pvalue_block_filename=PVALUE_FOLDER+f'{dataset_name}_dico{d}_pvalues_blocks_{counter}.pkl'
+                pvalue_flux_filename=PVALUE_FOLDER+f'{dataset_name}_dico{d}_pvalues_fluxes_{counter}.pkl'
+                counter+=1
+                        
             block_dict, flux_dict=validate(el_dico[d], qdecm, n_runs=N_RUNS, verbose=True)
             with open(pvalue_block_filename, 'wb') as f:
                 pickle.dump(block_dict, f)
